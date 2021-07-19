2c2a61b82b5734088f083741fd24095adbad471b
bbrauzzi <blasco.brauzzi@terradue.com>
2021-07-19 15:52:57 +0200

    pip path fix

--- conda.recipe/post-link.sh
+++ conda.recipe/post-link.sh
@@ -1,6 +1,6 @@
 
-$PREFIX/pip install eoepca-scim==2.7.4
+$PREFIX/bin/pip install eoepca-scim==2.7.4
 
-$PREFIX/pip install robotframework==3.2.2
+$PREFIX/bin/pip install robotframework==3.2.2
 
-$PREFIX/pip install WellKnownHandler==0.2.0
+$PREFIX/bin/pip install WellKnownHandler==0.2.0

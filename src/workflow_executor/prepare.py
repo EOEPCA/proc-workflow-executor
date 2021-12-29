aa3e5e8005c8e8bcb6b77d1664cfce8e957dc9af
bbrauzzi <blasco.brauzzi@terradue.com>
2021-12-29 13:10:05 +0100

    create_namespaced_secret fix

--- src/workflow_executor/prepare.py
+++ src/workflow_executor/prepare.py
@@ -275,6 +275,7 @@ def run(
 
             time.sleep(5)
             try:
+                secret_export.metadata.namespace = namespace
                 api_response = v1.create_namespaced_secret(
                     namespace, secret_export, pretty=pretty
                 )

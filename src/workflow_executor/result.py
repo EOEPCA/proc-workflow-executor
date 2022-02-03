5744227fad1b8584156d55000c5ff0752f562369
bbrauzzi <blasco.brauzzi@terradue.com>
2022-02-03 21:26:14 +0100

    view result pod fix

--- src/workflow_executor/result.py
+++ src/workflow_executor/result.py
@@ -1,3 +1,4 @@
+import os
 import sys
 import time
 from os import path
@@ -36,10 +37,10 @@ def run(
             "workflowname": workflowname,
             "outputfile": outputfile,
             "outputVolumeClaimName": f"{volume_name_prefix}-output-data",
+            "calrissianImage": os.getenv("CALRISSIAN_IMAGE", "terradue/calrissian:0.10.0")
         }
 
     yaml_modified = template.render(variables)
-
     body = yaml.safe_load(yaml_modified)
     pprint(body)
 

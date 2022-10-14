9fb8248fb991faefa557248d9dcb63dcfad6aebe
bbrauzzi <blasco.brauzzi@terradue.com>
2022-10-14 16:58:22 +0200

    EOEPCA-719 #comment _USERNAME env variable added

--- src/workflow_executor/fastapiserver.py
+++ src/workflow_executor/fastapiserver.py
@@ -258,6 +258,7 @@ def read_execute(content: ExecuteContent, response: Response):
         # retrieving rm endpoint and user
         resource_manager_endpoint = os.getenv("RESOURCE_MANAGER_ENDPOINT", None)
         resource_manager_user = content.username
+        pod_env_vars["_USERNAME"] = resource_manager_user
 
         platform_domain = os.getenv("ADES_PLATFORM_DOMAIN", None)
 

73e999b3c4da1414e1085e7f80dfe4964f40a2fe
bbrauzzi <blasco.brauzzi@terradue.com>
2023-03-28 14:07:04 +0200

    EOEPCA-864 #comment updated unit tests

--- src/workflow_executor/test/s3_url_test.py
+++ src/workflow_executor/test/s3_url_test.py
@@ -47,5 +47,23 @@ class MyTestCase(unittest.TestCase):
         self.assertEqual(s3_url, "s3://dev-test")
 
 
+
+    def test_S3_url_with_empty_projectid(self):
+
+        test_folder_path = path.dirname(__file__)
+        s3_access_json_file = path.join(test_folder_path, f"data/s3_access_test/s3_access_with_empty_projectId.json")
+
+        with open(s3_access_json_file, "r") as stream:
+            s3_access_json = json.load(stream)
+
+        bucketname = s3_access_json["storage"]["credentials"]["bucketname"]
+        projectid = s3_access_json["storage"]["credentials"]["projectid"] if "projectid" in s3_access_json["storage"]["credentials"] else None
+
+
+        s3_url = f"s3://{ projectid+':' if projectid else ''}{bucketname}"
+
+        self.assertEqual(s3_url, "s3://dev-test")
+
+
 if __name__ == '__main__':
     unittest.main()
